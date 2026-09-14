09ca4a13d85d7ce4d338c980e9d1ff4a3fd6bbeb
Ammar Waleed <ongo9972@gmail.com>
2026-09-14 16:29:45 +0300

    Correct comments and log output in reduce function
    Fix typos and improve comments in reduce example.

--- Higher Order Function/Reduce/script.js
+++ Higher Order Function/Reduce/script.js
@@ -4,15 +4,31 @@
 ---Return the restult in a single output value
 */
 
-
+//Without initial value
+//accumelator = 10[first array value]
 let nums = [10, 20, 15, 30];
 
+// let add = nums.reduce((acc, current, index, array)=>{
+//     console.log(`The accumelated value is:    [${acc}]`);//10
+//     console.log(`The Current Number is:       [${current}]`);
+//     console.log(`The Current Number Index is: [${index}]`);
+//     console.log("=======================");
+//     return acc + current;
+// })
+
+// console.log(add);
+
+
+//With initial value
+//accumelator = [initial value]
+
+
 let add = nums.reduce((acc, current, index, array)=>{
-    console.log(`The accumelated value is:    [${acc}]`);
+    console.log(`The accumelated value is:    [${acc}]`);//5
     console.log(`The Current Number is:       [${current}]`);
     console.log(`The Current Number Index is: [${index}]`);
     console.log("=======================");
     return acc + current;
-})
+}, 5)
 
 console.log(add);
