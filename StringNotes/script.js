1d4db2c18fcf46971b7c6bb8857e88139fac9bc9
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 05:04:45 +0300

    Add string usage examples to script.js
    Add examples of correct and incorrect string usage in JavaScript.

---
+++ StringNotes/script.js
@@ -0,0 +1,7 @@
+//[Notes: ]
+console.log("Hello 'ammar'");   [correct]
+console.log('Hello "ammar"');   [correct]
+//"Double quote inside single quote or single quote inside double quote " Normal
+//but double quote inside double quote is not normal and throws errors
+
+console.log("Hello "ammar"");   [error]
