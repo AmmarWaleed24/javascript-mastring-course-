d572254521e2ea9bdd72392df6ef78988609a371
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 05:09:31 +0300

    Add Number utility functions and examples

---
+++ Number/script.js
@@ -0,0 +1,22 @@
+//Double Precision
+//Syntactic Sugar
+//e
+//**
+
+
+console.log(typeof 10) //number
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10**6);
+// Number + BigInt
+// Number Min Value
+// Number Max Value
+console.log(Number(0.999).toFixed());//1
+console.log(Number("1000"));//1000
+//[Number] is a function
+//typeof Number => function
+console.log(Number.MAX_SAFE_INTEGER);//max safe integer
+console.log(Number.MAX_VALUE);//max value ever
+console.log(Number.MAX_VALUE+ 21212);
+//will not add any thing it's the max value
