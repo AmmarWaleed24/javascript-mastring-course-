7d9648cd82109974fc25e1ea8b769bce63bac1fb
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 05:07:00 +0300

    Implement template literals example in script.js
    Added an example of using template literals in JavaScript.

---
+++ Template Literals/script.js
@@ -0,0 +1,19 @@
+//Template Literals (Template Strings)
+
+let a  = "ammar"
+let b = "waleed"
+
+console.log(`${a}  ${b}`);
+
+Example:
+
+let markUp = `
+    <div class="bx">
+        <div class="container">
+            <h2>Ammar</h2>
+            <p>Hello From HTML File</p>
+        </div>
+    </div>
+`;
+
+document.write(markUp)
