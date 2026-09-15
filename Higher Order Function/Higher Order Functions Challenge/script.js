56882d2e0affce8d0553e29b6770edda818e8d05
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 04:45:22 +0300

    Higher Order Functions Challenge
    Higher Order Functions Challenge

---
+++ Higher Order Function/Higher Order Functions Challenge/script.js
@@ -0,0 +1,29 @@
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+solution
+let solution = myString.split(",").filter((ele)=>{
+  return isNaN(parseInt(ele)) ? ele : "";
+}).join("").slice(1, -1).split("_").join(" ");
+
+console.log(solution);//Elzero Web School
+
+
