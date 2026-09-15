f2f8068f0f7302662d04bb2e2f781035f8b6ad81
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 05:02:54 +0300

    Create script.js with console method examples
    Add examples of console methods in script.js

---
+++ consoleMethods/script.js
@@ -0,0 +1,12 @@
+//---log
+//---error
+//---table
+
+//Examples
+
+console.log("ammar")
+console.error("Error")
+console.table(["ammar", "ahmed", "osama zero"])
+
+//Note:
+//--console is not part from js it's "Web API"
