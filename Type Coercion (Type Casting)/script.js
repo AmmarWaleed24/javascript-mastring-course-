7ea37ae064276bb51c6d86e835f26eaf71d1576e
Ammar Waleed <ongo9972@gmail.com>
2026-09-15 05:08:20 +0300

    Add examples of type coercion in script.js
    Demonstrate type coercion and type casting in JavaScript with examples.

---
+++ Type Coercion (Type Casting)/script.js
@@ -0,0 +1,15 @@
+let a = "10";
+let b = 20;
+let c = true;
+
+console.log(a+b)//           Concate
+console.log(a-b)//           Type Coercion
+console.log(""+2)//          2
+console.log(+"")//           0
+console.log(true - false) // 1
+console.log( 10 - "ammar")// NaN
+console.log(typeof NaN)  //  Number
+
+let num = 1;
+console.log(++num); add first then print
+console.log(num++); print first then add
