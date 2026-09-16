e68b5bbbf949dfbf32f675ae379e04956b93792c
Ammar Waleed <ongo9972@gmail.com>
2026-09-17 01:58:26 +0300

    Add script for object creation examples
    Created a script to demonstrate object creation using both literal notation and the Object constructor.

---
+++ Object/ObjectPartFour/script.js
@@ -0,0 +1,24 @@
+/**
+ * --Object
+ * ---Create Object with a new Object();
+ */
+
+//             Notes
+
+let user = {
+    age: 20
+}
+
+user.uName = "Ammar";
+user["country"] = "Egypt";
+
+console.log(user.country);
+
+
+//           Object()
+
+let userTwo = new Object();
+userTwo.age = 30;
+userTwo.uName = "Ahmed";
+
+console.log(userTwo.uName);
