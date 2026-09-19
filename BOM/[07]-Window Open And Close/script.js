f0fff541a9a19b46e24a8f125b23effe6667bf64
Ammar Waleed <ongo9972@gmail.com>
2026-09-19 19:37:22 +0300

    Add window open/close functionality in script.js
    Implement window open and close functionality with timeouts and event listeners for Google and Facebook links.

---
+++ BOM/[07]-Window Open And Close/script.js
@@ -0,0 +1,33 @@
+/*
+  BOM [Browser Object Model]
+  - open(URL [Opt], Window Name Or Target Attr [Opt], Win Features [Opt], History Replace [Opt])
+  - close()
+  - Window Features
+  --- left [Num]
+  --- top [Num]
+  --- width [Num]
+  --- height [Num]
+  --- menubar [yes || no]
+
+  Search
+  - Window.Open Window Features
+*/
+
+setTimeout(function () {
+  window.open("", "_self", "", false);
+}, 2000);
+
+setTimeout(function () {
+  window.open("https://google.com", "_blank", "width=400,height=400,left=200,top=10");
+}, 2000);
+
+const google = document.querySelector(".google");
+const facebook = document.querySelector(".facebook");
+
+google.addEventListener("click", () => {
+  window.open("https://google.com", "_blank");
+});
+
+facebook.addEventListener("click", () => {
+  window.open("https://facebook.com", "_blank");
+});
